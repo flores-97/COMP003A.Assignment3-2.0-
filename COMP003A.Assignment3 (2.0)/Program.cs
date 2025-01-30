e8a25fde931ac8fabc81c98aabf1f0f0830dbf1d
flores-97 <floresvictor15@gmail.com>
2025-01-29 20:40:08 -0800

    Add project files.

---
+++ COMP003A.Assignment3 (2.0)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.Assignment3__2._0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
